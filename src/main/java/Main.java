79cd636e210c1f19b4df5d1e2bdb09070b4c3dc7
ErdalErdem <erda0028@stud.kea.dk>
2023-01-06 09:27:16 +0100

    Ovelse 1 done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+
+Tekst tekst = new Tekst();
+System.out.println("Tekstlinjer:" + tekst);
+
+tekst.tilføj("hej");
+tekst.tilføj("hej");
+tekst.tilføj("jan");
+tekst.tilføj("dig");
+        System.out.println("Tekstlinjer:" + tekst);
+
+int antalUnikke = tekst.findAntalUnikke();
+        System.out.println("Antal unikke:" + tekst.findAntalUnikke());
+
+
+
+
+    }
 }
--- src/main/java/Tekst.java
+++ src/main/java/Tekst.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Tekst {
+import java.util.ArrayList;
+
+public class Tekst {
+    private ArrayList<String> tekstlinjer = new ArrayList<String>();
+
+    public void tilføj(String tekst) {
+        tekstlinjer.add(tekst);
+
+    }
+
+    public int findAntalUnikke() {
+        ArrayList<String> unikListe = new ArrayList<>();
+        for (String s : tekstlinjer) {
+            if (!unikListe.contains(s))
+            unikListe.add(s);
+        }
+return unikListe.size();
+
+    }
+
+    @Override
+    public String toString() {
+        return "" + tekstlinjer;
+    }
 }
+
+
